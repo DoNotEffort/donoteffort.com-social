6d658a1f95e4768be745fb8036ab523bb446c92a
Onur Ayçiçek <onur.m.aycicek@gmail.com>
2022-12-15 15:47:48 +0300

    deleted user page

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,10 @@ import defaultLayout from '../layouts/default'
 
 import homePage from '../pages/index'
 
-import user from './user';
-
 export default ()=>[
 			{
 				path: '/',
 				name: 'home',
 				component: homePage
 			},
-				...user(),
 ]
