d19b126970f29aae30255bdf14c021c0f46dda09
Onur Ayçiçek <onur.m.aycicek@gmail.com>
2022-12-18 03:59:29 +0300

    deleted unnecessary modules and files

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,8 +52,6 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
     '@nuxtjs/i18n',
@@ -64,12 +62,6 @@ export default {
     'cookie-universal-nuxt'
   ],
 
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
